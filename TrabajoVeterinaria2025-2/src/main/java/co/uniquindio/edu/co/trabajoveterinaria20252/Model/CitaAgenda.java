b849696a54faf1316fb5c1880ba791ecf74ba2ed
Juan Manuel Velasquez Lenis <juanm.velasquezl@uqvirtual.edu.co>
2025-08-15 13:12:09 -0500

    Prueba

--- TrabajoVeterinaria2025-2/src/main/java/co/uniquindio/edu/co/trabajoveterinaria20252/Model/CitaAgenda.java
+++
@@ -1,4 +0,0 @@
-package co.uniquindio.edu.co.trabajoveterinaria20252.Model;
-
-public class CitaAgenda {
-}
---
+++ TrabajoVeterinaria2025-2/src/main/java/co/uniquindio/edu/co/trabajoveterinaria20252/Model/CitaAgendada.java
@@ -0,0 +1,57 @@
+package co.uniquindio.edu.co.trabajoveterinaria20252.Model;
+
+import java.time.LocalDate;
+
+
+public class CitaAgendada {
+    private LocalDate fecha;
+    private String motivo;
+    private Mascota mascotaAtendida;
+    private Persona veterinarioAsignado;
+
+    public CitaAgendada(LocalDate fecha, String motivo, Mascota mascotaAtendida, Persona veterinarioAsignado) {
+        this.fecha = fecha;
+        this.motivo = motivo;
+        this.mascotaAtendida = mascotaAtendida;
+        this.veterinarioAsignado = veterinarioAsignado;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getMotivo() {
+        return motivo;
+    }
+
+    public void setMotivo(String motivo) {
+        this.motivo = motivo;
+    }
+
+    public Mascota getMascotaAtendida() {
+        return mascotaAtendida;
+    }
+
+    public void setMascotaAtendida(Mascota mascotaAtendida) {
+        this.mascotaAtendida = mascotaAtendida;
+    }
+
+    public Persona getVeterinarioAsignado() {
+        return veterinarioAsignado;
+    }
+
+    public void setVeterinarioAsignado(Persona veterinarioAsignado) {
+        this.veterinarioAsignado = veterinarioAsignado;
+    }
+
+    @Override
+    public String toString() {
+        return "fecha: " + fecha +
+                ", motivo:" + motivo + '\'' +
+                ", mascotaAtendida: " + mascotaAtendida;
+    }
+}
